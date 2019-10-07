d39911d56e64047acbf79ccdf5aa2ea8b9ef5bb2
Sandy <46079394+sdyl3@users.noreply.github.com>
2019-10-06 22:47:26 -0700

    Update Die.cpp

--- Pig/Die.cpp
+++ Pig/Die.cpp
@@ -3,22 +3,20 @@
 #include "Die.h"
 #include <random>
 
-namespace cs {
-    Die::Die( int sides ) : mSides( sides ), mValue( 1 ) {
+Die::Die( int sides ) : mSides( sides ), mValue( 1 ) {
 
-    }
+}
     
-    // generates a random distribution of values between 1 and mSides
-    // stores the random value in the member variable mValue for later use
-    void Die::roll() {
-        std::random_device rd;
-        std::mt19937 e2(rd());
-        std::uniform_int_distribution<> dist(1, mSides);
-        mValue = dist(e2);
-    }
+// generates a random distribution of values between 1 and mSides
+// stores the random value in the member variable mValue for later use
+void Die::roll() {
+    std::random_device rd;
+    std::mt19937 e2(rd());
+    std::uniform_int_distribution<> dist(1, mSides);
+    mValue = dist(e2);
+}
     
-    // returns the most recent tossed value
-    int  Die::getValue( ) const {
-        return( mValue );
-    }
+// returns the most recent tossed value
+int  Die::getValue( ) const {
+    return( mValue );
 }
