afd88183df2b18d090ff35a86ef223f7edb9107b
Sandy <46079394+sdyl3@users.noreply.github.com>
2019-10-06 22:54:45 -0700

    Update Player.cpp

--- Pig/Player.cpp
+++ Pig/Player.cpp
@@ -1,70 +1,43 @@
-//
-//  Player.cpp
-//  Pig
-//
-//
+// Player.cpp
 
 #include "Player.h"
-
 #include <iostream>
-
-namespace cs31
-{
     
-    Player::Player() : mDie( 6 ), mScore( 0 ), mTotal( 0 )
-    {
+Player::Player() : mDie( 6 ), mScore( 0 ), mTotal( 0 ) {
         
-    }
+}
     
-    // TODO: roll the die and adjust the score for the current round
-    // accordingly
-    // when the amount is zero, the Die should be rolled for random play
-    // when the amount is not zero, we are trying to cheat for testing
-    // purposes.  in that case, this method should force a specific roll
-    // amount.
-    int Player::roll( int amount )
-    {
-        // stubbed out for now...
-        int rolledNumer;
-        if(amount == 0)
-        {
-            mDie.roll();
-            rolledNumer = mDie.getValue();
-        }
-        else
-        {
-            rolledNumer = amount;
-        }
-        mScore += rolledNumer;
-        if (rolledNumer == 1)
-        {
-            mScore = 0;
-        }
-        return rolledNumer;
+// rolls the die and adjusts the score for the current round accordingly
+// when the amount is zero, the Die should be rolled for random play
+// when the amount is not zero, we are trying to cheat for testing purposes.  
+// in that case, this method should force a specific roll amount.
+int Player::roll( int amount ) {
+    int rolledNumer;
+    if(amount == 0) {
+        mDie.roll();
+        rolledNumer = mDie.getValue();
     }
-    
-    // TODO: add the current score into the running total since the turn ended
-    //       reset the current score to 0 for the next round of play
-    void Player::endTurn()
-    {
-        // stubbed out for now...
-        mTotal += mScore;
+    else {
+        rolledNumer = amount;
+    }
+    mScore += rolledNumer;
+    if (rolledNumer == 1) {
         mScore = 0;
     }
+    return rolledNumer;
+}
     
-    // TODO: return the current total
-    int Player::getTotal( ) const
-    {
-        // stubbed out for now...
-        return mTotal;
-    }
+// adds the current score into the running total since the turn ended
+// reset the current score to 0 for the next round of play
+void Player::endTurn(){
+    mTotal += mScore;
+    mScore = 0;
+}
     
-    // TODO: return the current total
-    int  Player::getScore( ) const
-    {
-        // stubbed out for now...
-        return mScore;
-    }
+int Player::getTotal( ) const {
+    return mTotal;
+}
     
-
+int  Player::getScore( ) const {
+    return mScore;
 }
