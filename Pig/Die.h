894769382f1d413ad4d0cb9fa45d857aed833344
Sandy <46079394+sdyl3@users.noreply.github.com>
2019-10-06 22:48:05 -0700

    Update Die.h

--- Pig/Die.h
+++ Pig/Die.h
@@ -1,15 +1,18 @@
 //  Die.h
+
 #ifndef DIE_H
 #define DIE_H
 
 // this class simulates a random Die toss.  
 // When roll( ) is called, a value between 1 - mSides will be calculated and stored.
 // Retrieve the value rolled by calling getValue( ).
+
 class Die {
 public:
     Die( int sides = 6 );   // by default, a six sided die
     void roll();            // tossing the die, updating mValue
     int  getValue( ) const; // retrieve the value that was just tossed
+
 private:
     int  mSides;            // how many sides are on this die?
     int  mValue;            // the value of the most recent toss
